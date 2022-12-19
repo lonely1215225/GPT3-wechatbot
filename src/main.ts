922c8dd9acd506b03e2c58e493f081d40ab1c936
zhangzhuoyue <zhangzhuoyue@coding.com>
2022-12-19 18:01:22 +0800

    welcome to

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,57 @@
 import QRCode from "qrcode";
-import { WechatyBuilder } from "wechaty";
-import { ChatGPTBot } from "./chatgpt.js";
+import {WechatyBuilder} from "wechaty";
+import {ChatGPTBot} from "./chatgpt.js";
 
 // Wechaty instance
 const weChatBot = WechatyBuilder.build({
-  name: "my-wechat-bot",
+    name: "my-wechat-bot",
 });
 // ChatGPTBot instance
 const chatGPTBot = new ChatGPTBot();
 
 async function main() {
-  weChatBot
-    // scan QR code for login
-    .on("scan", async (qrcode, status) => {
-      const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
-      console.log(`💡 Scan QR Code to login: ${status}\n${url}`);
-      console.log(
-        await QRCode.toString(qrcode, { type: "terminal", small: true })
-      );
-    })
-    // login to WeChat desktop account
-    .on("login", async (user: any) => {
-      console.log(`✅ User ${user} has logged in`);
-      chatGPTBot.setBotName(user.name());
-      await chatGPTBot.startGPTBot();
-    })
-    // message handler
-    .on("message", async (message: any) => {
-      try {
-        console.log(`📨 ${message}`);
-        // add your own task handlers over here to expand the bot ability!
-        // e.g. if a message starts with "Hello", the bot sends "World!"
-        if (message.text().startsWith("Hello")) {
-          await message.say("World!");
-          return;
-        }
-        // handle message for chatGPT bot
-        await chatGPTBot.onMessage(message);
-      } catch (e) {
-        console.error(`❌ ${e}`);
-      }
+    weChatBot
+        // scan QR code for login
+        .on("scan", async (qrcode, status) => {
+            const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
+            console.log(`💡 Scan QR Code to login: ${status}\n${url}`);
+            console.log(
+                await QRCode.toString(qrcode, {type: "terminal", small: true})
+            );
+        })
+        // login to WeChat desktop account
+        .on("login", async (user: any) => {
+            console.log(`✅ User ${user} has logged in`);
+            chatGPTBot.setBotName(user.name());
+            await chatGPTBot.startGPTBot();
+        })
+        // message handler
+        .on("message", async (message: any) => {
+            try {
+                console.log(`📨 ${message}`);
+                // add your own task handlers over here to expand the bot ability!
+                // e.g. if a message starts with "Hello", the bot sends "World!"
+                if (message.text().startsWith("Hello")) {
+                    await message.say("World!");
+                    return;
+                }
+                // handle message for chatGPT bot
+                await chatGPTBot.onMessage(message);
+            } catch (e) {
+                console.error(`❌ ${e}`);
+            }
+        }).on('room-join', async function (room, inviteeList, inviter) {
+        await chatGPTBot.onInviteIn(room,inviteeList,inviter);
     });
 
-  try {
-    await weChatBot.start();
-  } catch (e) {
-    console.error(`❌ Your Bot failed to start: ${e}`);
-    console.log(
-      "🤔 Can you login WeChat in browser? The bot works on the desktop WeChat"
-    );
-  }
+    try {
+        await weChatBot.start();
+    } catch (e) {
+        console.error(`❌ Your Bot failed to start: ${e}`);
+        console.log(
+            "🤔 Can you login WeChat in browser? The bot works on the desktop WeChat"
+        );
+    }
 }
+
 main();
